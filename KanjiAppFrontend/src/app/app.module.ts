5c115dd018905ffe30650d364727ff1937d68e16
DDSNA <79444147+DDSNA@users.noreply.github.com>
2023-04-15 11:01:09 +0300

    New component: title and paragraph. //Angular is strange

--- KanjiAppFrontend/src/app/app.module.ts
+++ KanjiAppFrontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { KanjiListComponent } from './kanji-list/kanji-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KanjiListComponent
   ],
   imports: [
     BrowserModule
