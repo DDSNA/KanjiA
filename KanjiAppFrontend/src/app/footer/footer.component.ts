b5656553e05eadc4de1b8140eb35f519527cde57
Dan <79444147+DDSNA@users.noreply.github.com>
2023-04-23 09:33:04 +0300

    adapting too bootstrap & css grid

--- KanjiAppFrontend/src/app/footer/footer.component.ts
+++ KanjiAppFrontend/src/app/footer/footer.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   allowFormRefresh = false;
+  handleClick(){
+    setTimeout(() => {
+      this.allowFormRefresh = false;
+    }, 2000);
+  }
 
 constructor() { 
   setTimeout(() => {
