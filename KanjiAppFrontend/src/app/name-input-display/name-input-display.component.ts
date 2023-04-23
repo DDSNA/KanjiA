fb35ceb61b9a4b9856df1f87a0513e4aba8ef56e
Dan <79444147+DDSNA@users.noreply.github.com>
2023-04-23 12:22:25 +0300

    check your paranthesis kids

--- KanjiAppFrontend/src/app/name-input-display/name-input-display.component.ts
+++ KanjiAppFrontend/src/app/name-input-display/name-input-display.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-name-input-display',
   templateUrl: './name-input-display.component.html',
   styleUrls: ['./name-input-display.component.css']
 })
-export class NameInputDisplayComponent {
+export class NameInputDisplayComponent implements OnInit {
 
   Username='Bia';
 
   onUpdateUsername(event: Event) {
     this.Username= (<HTMLInputElement>event.target).value;
+  }
+  
+  ngOnInit() {
   }
 
 }
